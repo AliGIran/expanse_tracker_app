2cceddf94b35ea7e919e2dee93c8c9b3686ace12
AliGIran <a.gorji013@gmail.com>
2024-06-26 13:32:59 +0330

    Sign up mockup page finished

--- lib/login/presentation/pages/login_sign_up_page.dart
+++ lib/login/presentation/pages/login_sign_up_page.dart
@@ -1,8 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 
-class LoginSignUpPage extends StatelessWidget {
+class LoginSignUpPage extends StatefulWidget {
   const LoginSignUpPage({super.key});
+
+  @override
+  State<LoginSignUpPage> createState() => _LoginSignUpPageState();
+}
+
+class _LoginSignUpPageState extends State<LoginSignUpPage> {
+  TextEditingController nameTextEditingController = TextEditingController();
+
+  TextEditingController emailTextEditingController = TextEditingController();
+
+  TextEditingController passwordTextEditingController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -10,35 +21,68 @@ class LoginSignUpPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Sign Up"),
         centerTitle: true,
-        leading: BackButton(),
+        leading: const BackButton(),
       ),
       body: SingleChildScrollView(
         // scrollDirection: Axis.horizontal,
-        physics: AlwaysScrollableScrollPhysics(),
+        physics: const AlwaysScrollableScrollPhysics(),
         child: Center(
           child: Container(
-            padding: EdgeInsets.all(16),
+            padding: const EdgeInsets.all(16),
             child: Column(
               children: [
                 const Gap(25),
+
                 TextField(
-                  decoration: InputDecoration(
+                  controller: nameTextEditingController,
+                  decoration: const InputDecoration(
                       labelText: "Name", hintText: 'Enter your name'),
                 ),
+
                 const Gap(35),
+
                 TextField(
-                  decoration: InputDecoration(
+                  controller: emailTextEditingController,
+                  decoration: const InputDecoration(
                       labelText: "Email", hintText: 'Enter your email'),
                 ),
+
                 const Gap(35),
+
                 TextFormField(
+                  controller: passwordTextEditingController,
                   decoration: InputDecoration(
                       labelText: 'Password',
                       hintText: 'Enter your password',
                       suffixIcon: IconButton(
-                          onPressed: () {}, icon: Icon(Icons.remove_red_eye))),
+                          onPressed: () {},
+                          icon: const Icon(Icons.remove_red_eye))),
                 ),
-                const Gap(35),
+
+                Container(
+                  margin: const EdgeInsets.only(top: 30),
+                  child: ListTile(
+                      leading: Checkbox(
+                        value: false,
+                        onChanged: (bool? value) {
+                          value = true;
+                        },
+                      ),
+                      title: RichText(
+                        text: const TextSpan(
+                          text: 'By signing up, you agree to the  ',
+                          style: TextStyle(color: Colors.black),
+                          children: <TextSpan>[
+                            TextSpan(
+                              text: 'Terms of Service and Privacy Policy',
+                              style: TextStyle(color: Colors.purpleAccent),
+                            ),
+                          ],
+                        ),
+                      )),
+                ),
+
+                const Gap(15),
 
                 //Sign up button
                 SizedBox(
@@ -51,15 +95,24 @@ class LoginSignUpPage extends StatelessWidget {
                   "Or with",
                   style: TextStyle(color: Colors.grey),
                 ),
-
+                const Gap(30),
                 //todo: placeholder must be replaced by google account
                 const Placeholder(
                   fallbackHeight: 40,
                 ),
 
-                const Text(
-                  "Already have an account? Login",
-                  style: TextStyle(color: Colors.grey),
+                const Gap(30),
+                RichText(
+                  text: const TextSpan(
+                      text: 'Already have an account?',
+                      style: TextStyle(color: Colors.grey),
+                      children: [
+                        TextSpan(
+                            text: 'LogIn',
+                            style: TextStyle(
+                              color: Colors.deepPurple,
+                            ))
+                      ]),
                 )
               ],
             ),
