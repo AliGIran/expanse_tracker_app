bf93848967d1b55774fd3f5b35e3b1c23a780964
AliGIran <a.gorji013@gmail.com>
2024-06-30 14:13:52 +0330

    test barzzegar

--- lib/draft.dart
+++ lib/draft.dart
@@ -1,66 +1,19 @@
 import 'package:flutter/material.dart';
 
-class Draft extends StatelessWidget {
+class Draft extends StatefulWidget {
   const Draft({super.key});
 
+  @override
+  State<Draft> createState() => _DraftState();
+}
+
+class _DraftState extends State<Draft> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Draft"),
-        centerTitle: true,
+        title: const Text("Barbarize"),
       ),
-      body: Center(
-        child: FilledButton(
-          onPressed: () => showModalSheet(context),
-          child: const Text("Show modal sheet"),
-        ),
-      ),
-    );
-  }
-
-  showModalSheet(BuildContext context) {
-    return showModalBottomSheet<void>(
-      showDragHandle: true,
-      context: context,
-      builder: (context) {
-        return SizedBox(
-          height: MediaQuery.of(context).size.height * 0.25,
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Text(
-                  "Logout?",
-                  style: Theme.of(context).textTheme.headlineSmall,
-                ),
-                const Text(
-                  "Are you sure do you wanna logout?",
-                  style: TextStyle(color: Colors.grey),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Expanded(
-                      child: Container(
-                        margin: const EdgeInsets.only(left: 8, right: 16),
-                        child: FilledButton.tonal(
-                            onPressed: () {}, child: const Text("No")),
-                      ),
-                    ),
-                    Expanded(
-                        child: Container(
-                      margin: const EdgeInsets.only(left: 16, right: 8),
-                      child: FilledButton(
-                          onPressed: () {}, child: const Text("Yes")),
-                    )),
-                  ],
-                ),
-              ],
-            ),
-          ),
-        );
-      },
     );
   }
 }
